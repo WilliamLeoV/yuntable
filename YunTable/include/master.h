7d3808e17c40dcee6ff81e10af646d25fba749fc
ikewu83 <ikewu83@a26f21ce-7369-1176-bc8b-9f905022af7f>
2011-01-07 09:42:16 +0000

    版本号0.9 Updated the Master code base on the new Change, and remove all code related with Column Family, besides the logging also has been setup.

--- YunTable/include/master.h
+++ YunTable/include/master.h
@@ -4,17 +4,15 @@
 #include "global.h"
 #include "conf.h"
 
-#define DEFAULT_MASTER_PORT 8301
-#define DEFAULT_MASTER_CONF_PATH "conf/master.conf"
+/** The below methods just used for reference, won't directly called **/
 
 #define GET_TABLE_INFO_MASTER_CMD "get_table_info_master"
 
 public TableInfo* get_table_info_master(char* table_name);
 
-#define REFRESH_TABLE_INFO_NOW_MASTER_CMD "refresh_table_info_now_master"
+#define CHECK_PROBLEM_REGION_MASTER_CMD "check_problem_region"
 
-/* This cmd will ask the master to check the problem node immediately */
-public boolean refresh_region_status_now_master(char* problem_region_conn);
+public boolean check_problem_region_master(char* problem_region_conn);
 
 #define ADD_NEW_REGION_MASTER_CMD "add_new_region_master"
 
@@ -23,10 +21,6 @@ public boolean add_new_region_master(char* region_conn);
 #define CREATE_NEW_TABLE_MASTER_CMD "create_new_table_master"
 
 public boolean create_new_table_master(char* table_name);
-
-#define ADD_NEW_COLUMN_FAMILY_MASTER_CMD "add_new_column_family_master"
-
-public boolean add_new_column_family_master(char* table_name, char* column_family);
 
 
 #endif /* MASTER_H_ */
