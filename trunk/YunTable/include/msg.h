2291bf25faa6354c3e866a17a3bfd14c3401bfc4
ikewu83 <ikewu83@a26f21ce-7369-1176-bc8b-9f905022af7f>
2011-01-07 09:45:22 +0000

    版本号0.9 Update some new msg

--- trunk/YunTable/include/msg.h
+++ trunk/YunTable/include/msg.h
@@ -6,16 +6,14 @@
  **/
 
 /** Error Messages **/
+#define ERR_MSG_CMD_NOT_COMPLETE "Please input the complete cmd."
 #define ERR_MSG_NULL_STRING "The input string is NULL or invalid."
 #define ERR_MSG_WRONG_ACTION "The action you just input is not supported."
 #define ERR_MSG_NO_COLUMN "No column has been inputed."
-#define ERR_MSG_NO_COLUMN_FAMILY "No column family has been inputed."
 #define ERR_MSG_NO_TABLE_NAME "No table name has been inputed."
 #define ERR_MSG_NO_ROW_KEY "Should have a row key."
 #define ERR_MSG_PUT "The Put Data action has failed."
 #define ERR_MSG_TABLE_NOT_CREATED "pLease create the table first."
-#define ERR_MSG_COLUMN_FAMILY_NOT_CREATED "pLease create the column family first."
-#define ERR_MSG_NEW_COLUMN_FAMILY_NOT_CREATED "The new column family has failed to be created."
 #define ERR_MSG_NO_CONF_FILE "Please input a conf file, such as ./startCli -conf conf/cli.conf."
 #define ERR_MSG_NO_MASTER "Please set up a master at first"
 #define ERR_MSG_CLUSTER_FULL "Can not get a new region for this table because the whole cluster is full."
@@ -25,11 +23,9 @@
 /** Issue Messages **/
 #define ISSUE_MSG_NOTHING_FOUND "Nothing has been found."
 #define ISSUE_MSG_TABLE_ALREADY_EXISTED "The table has already existed."
-#define ISSUE_MSG_COLUMN_FAMILY_ALREADY_EXISTED "The column family has already existed."
 
 /** Success Messages **/
 #define SUCC_MSG_COMPLETED "The Action has been completed successfully."
 #define SUCC_MSG_TABLE_CREATED "The new table has been created successfully."
-#define SUCC_MSG_NEW_COLUMN_FAMILY_CREATED "The new column family has been created successfully."
 
 #endif /* MSG_H_ */
