3856d90c51eca6ad5452df47f387efa271a09e30
ikewu83 <ikewu83@a26f21ce-7369-1176-bc8b-9f905022af7f>
2011-01-13 09:10:35 +0000

    Ver 0.9 Create a new script call clean.sh, that will remove all the data generated by YunTable.

--- trunk/YunTable/build.sh
+++ trunk/YunTable/build.sh
@@ -3,7 +3,6 @@ make cli -B
 make master -B
 make region -B
 echo "Removing Some Legacy Files!!!!!, it is also fine if reports some err msg."
-make clean
 
 cp conf/cli.conf.template conf/cli.conf
 cp conf/master.conf.template conf/master.conf
