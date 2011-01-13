59a40e8116998534d26984263877e3671da3af98
ikewu83 <ikewu83@a26f21ce-7369-1176-bc8b-9f905022af7f>
2011-01-13 09:15:23 +0000

    Ver 0.9 Remove unused stuff.

--- trunk/YunTable/build.sh
+++ trunk/YunTable/build.sh
@@ -2,8 +2,3 @@ echo "Compiling YunTable!!!!!"
 make cli -B
 make master -B
 make region -B
-echo "Removing Some Legacy Files!!!!!, it is also fine if reports some err msg."
-
-cp conf/cli.conf.template conf/cli.conf
-cp conf/master.conf.template conf/master.conf
-cp conf/region.conf.template conf/region.conf
