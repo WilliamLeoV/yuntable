19dcd1e07ca38205f03dfab1c4d606182a97528e
ikewu83 <ikewu83@a26f21ce-7369-1176-bc8b-9f905022af7f>
2011-01-25 15:17:50 +0000

    Ver0.9 Have done some updates on ITEM: 1. Add some comments on Item, which is the guideline for judging whether the item is deletion mark. 2. Update Create Key and Create Item method for handling NULL input. 3. Create a new method cmp_item_void, which will be called by qsort. 4. Update cmp_key_with_row_key and print_result_set_in_nice_format method. 5. Add remove duplication capabilty for m_combine_result_set. 6. Add cleansing method for processing the deletion mark from result set. 7. Add a new testcase for combine and cleansing result set.

--- YunTable/util/item.c
+++ YunTable/util/item.c
@@ -3,18 +3,19 @@
 #include "item.h"
 #include "buf.h"
 #include "malloc2.h"
+#include "log.h"
 
 struct _Key{
 		short row_key_len;
 		char* row_key;
-		short column_name_len;
-		char* column_name;
+		short column_name_len; /**If column_name_len == 0, means all columns before timestamp has been deleted **/
+		char* column_name; /**If the item is the deletion mark, the column name means the column before timestamp has been deleted **/
 		long long timestamp;
 };
 
 struct _Item{
         Key *key;
-        int val_len;
+        int val_len; /** If the val_len is 0, means the item is the deletion mark **/
         byte *value;
 };
 
@@ -23,7 +24,6 @@ struct _Item{
  * item_size
  * Item** -> key, val_len, value
  ***/
-
 #define RESULT_SET_MAGIC "rsutSet"
 
 public Key* get_key(Item *item){
@@ -42,12 +42,20 @@ public Key* m_load_key(FILE *fp){
 		return key;
 }
 
-private Key* create_key(char *row_key, char *column_qualifier){
+private Key* create_key(char *row_key, char *column_name){
+		if(row_key == NULL){
+			logg(ISSUE, "Insert Null Row key!!!!");
+		}
 		Key *key = malloc2(sizeof(Key));
 		key->row_key_len = strlen(row_key);
 		key->row_key = m_cpy(row_key);
-		key->column_name_len = strlen(column_qualifier);
-		key->column_name = m_cpy(column_qualifier);
+		if(column_name != NULL){
+			key->column_name_len = strlen(column_name);
+			key->column_name = m_cpy(column_name);
+		}else{
+			key->column_name_len = 0;
+			key->column_name = NULL;
+		}
 		key->timestamp = get_current_time_stamp();
 		return key;
 }
@@ -61,8 +69,13 @@ public Item* m_create_item(char *row_key, char *column_name, char *value){
 		//init a new item base on the input
 		Item *item = malloc2(sizeof(Item));
 		item->key = key;
-		item->val_len = strlen(value);
-		item->value = m_cpy(value);
+		if(value != NULL){
+			item->val_len = strlen(value);
+			item->value = m_cpy(value);
+		}else{
+			item->val_len = 0;
+			item->value = NULL;
+		}
 		return item;
 }
 
@@ -184,7 +197,7 @@ public ResultSet* byte_to_result_set(byte* bytes){
         resultSet->items = malloc2(sizeof(Item *) * resultSet->size);
         int i=0;
         for(i=0; i<resultSet->size; i++){
-                resultSet->items[i] = byte_to_item(buf);
+        	resultSet->items[i] = byte_to_item(buf);
         }
         return resultSet;
 }
@@ -197,7 +210,6 @@ public void free_item(Item* item){
         if(item == NULL) return;
         free_key(item->key);
         frees(2, item->value, item);
-
 }
 
 /* only free inside items */
@@ -219,15 +231,46 @@ public void destory_result_set(ResultSet *resultSet){
         frees(2, resultSet->items, resultSet);
 }
 
-public int cmp_item(const void* item1_void, const void* item2_void){
-        //first load 4bit of address, then converted to item pointer, and only tested at 32bit
-        const Item* item1 = (Item*)(*(long*)item1_void);
-        const Item* item2 = (Item*)(*(long*)item2_void);
-        return cmp_item_with_row_key(item1, item2->key->row_key);
+/** This method is used for qsort **/
+public int cmp_item_void(const void* item1_void, const void* item2_void){
+		  //first load 4byte of address, then converted to item pointer, and only tested at 32bit
+		  Item* const item1 = (Item*)(*(long*)item1_void);
+		  Item* const item2 = (Item*)(*(long*)item2_void);
+		  return cmp_item(item1, item2);
 }
 
+/** Will item base on row key, timestamp and column name, will return result if either one of them are not match
+ *  If the method return zero, means the two items are the same
+ * **/
+public int cmp_item(Item* item1, Item* item2){
+		int key_cmp = cmp_item_with_row_key(item1, item2->key->row_key);
+        if(key_cmp != 0){
+        	return key_cmp;
+        }
+        if(item1->key->timestamp == item2->key->timestamp){
+			char* column_name1 = item1->key->column_name;
+			char* column_name2 = item2->key->column_name;
+        	//The Column Name NULL is bigger than other Column Name
+			if(column_name1 == NULL && column_name2 == NULL){
+        		return 0;
+        	}else if(column_name1 == NULL){
+        		return 1;
+        	}else if(column_name2 == NULL){
+        		return -1;
+        	}else{
+        		return match_int(column_name1, column_name2);
+        	}
+        }else if(item1->key->timestamp > item2->key->timestamp){
+        	return 1;
+        }else{
+        	return -1;
+        }
+}
+
+/** If the two items share same row key, the method will return 0 **/
 public int cmp_key_with_row_key(const Key *key, const char* row_key){
         int max_len = max(key->row_key_len, strlen(row_key));
+        //Since the case is matter, so it not used the match method from Util
         return strncmp(key->row_key, row_key, max_len);
 }
 
@@ -260,14 +303,14 @@ public ResultSet* found_items_by_row_key(int size, Item** items, char* row_key){
                 //left rotate the index
                 int left_index = index - 1;
                 while(left_index >= 0 && cmp_item_with_row_key(items[left_index], row_key)==0){
-                        list_append(itemList, items[left_index]);
-                        left_index--;
+					list_append(itemList, items[left_index]);
+					left_index--;
                 }
                 //right rotate the index
                 int right_index = index + 1;
                 while(right_index < size && cmp_item_with_row_key(items[right_index], row_key)==0){
-                        list_append(itemList, items[right_index]);
-                        right_index++;
+					list_append(itemList, items[right_index]);
+					right_index++;
                 }
         }
         ResultSet* resultSet = m_item_list_to_result_set(itemList);
@@ -279,8 +322,8 @@ public ResultSet* found_items_by_timestamp(int size, Item** items, int begin_tim
         List* itemList = list_create();
         int i=0;
         for(i=0; i<size; i++){
-                if(between_timestamps(items[i]->key->timestamp, begin_timestamp, end_timestamp))
-                        list_append(itemList, items[i]);
+			if(between_timestamps(items[i]->key->timestamp, begin_timestamp, end_timestamp))
+				list_append(itemList, items[i]);
         }
         ResultSet* resultSet = m_item_list_to_result_set(itemList);
         list_destory(itemList, only_free_struct);
@@ -315,49 +358,160 @@ public void print_result_set_in_nice_format(ResultSet* resultSet){
 		int i=0;
 		for(i=0; i<resultSet->size; i++){
 			Item *item = resultSet->items[i];
-			printf("%s:%s timstamp:%lld\n", get_column_name(item), get_value(item), get_timestamp(item));
+			if(item != NULL){
+				printf("%s %s:%s timstamp:%lld\n", item->key->row_key, item->key->column_name,
+						item->value, item->key->timestamp);
+			}
 		}
 }
 
-public ResultSet* m_combine_result_set(ResultSet* set0, ResultSet* set1){
+/** The method will merge the and remove duplication, besides two inputed set needs be sorted,
+ *  but the return will keep item copy.
+ ***/
+public ResultSet* m_combine_result_set(ResultSet* leftSet, ResultSet* rightSet){
         //init new ResultSetArray
-        int total_size = set0->size + set1->size;
+        int total_size = leftSet->size + rightSet->size;
         Item** items = malloc2(sizeof(Item *) * total_size);
-        int i=0, j=0;
-        //append array0
-        for(i=0; i<set0->size; i++){
-                items[i] = set0->items[i];
+        int left = 0; //The Pointer for left ResultSet
+        int right = 0; //The Pointer for right ResultSet
+        int index = 0; //The Pointer of the new ResultSet
+        //The method will iterate after all two sets has been processed
+        while(left!=leftSet->size || right!=rightSet->size){
+        	//If the right set has been all processed
+        	if(right == rightSet->size){
+        		//the left set can immediately insert into the new set
+        		items[index] = leftSet->items[left];
+        		left++;
+        	}
+        	//If the left set has been all processed
+        	else if(left == leftSet->size){
+        		items[index] = rightSet->items[right];
+        		right++;
+        	}else{
+        		Item* leftItem = leftSet->items[left];
+        		Item* rightItem = rightSet->items[right];
+        		int cmp_result = cmp_item(leftItem, rightItem);
+        		//If the two items are the same, insert the left and discard right
+        		if(cmp_result == 0){
+        			items[index] = leftItem;
+        			left++;
+        			//If the val lens are same, means they are same
+        			if(leftItem->val_len == rightItem->val_len)
+        			right++;
+        		}
+        		//If left smaller than right
+        		else if(cmp_result < 0){
+        			items[index] = leftItem;
+        			left++;
+        		}
+        		//If right smalled than left
+        		else{
+        			items[index] = rightItem;
+        			right++;
+        		}
+        	}
+        	index++;
         }
-        //append array1
-        for(j=0; j<set1->size; j++, i++){
-                items[i] = set1->items[j];
-        }
-        ResultSet* newSet = m_create_result_set(total_size, items);
+        ResultSet* newSet = m_create_result_set(index, items);
         return newSet;
 }
 
+/**
+ * 	Removing deleted item from result set, and return result set will have some empty slots.
+ *  TODO This method contains some duplication code.
+ * */
+public ResultSet* cleansing(ResultSet* set){
+		int i;
+		for(i=0; i<set->size; i++){
+			Item* item = set->items[i];
+			//check if the item is the row key deletion mark,
+			//which will delete the row key's all items before this item's timestamp
+			if(item->val_len == 0 && item->key->column_name_len ==0){
+				int j=0;//The reverse index, the first item gonna be the item before current deletion mark
+				for(j=i-1; j>=0; j--){
+					Item *prevItem = set->items[j];
+					if(prevItem == NULL) continue;
+					//The procedure will break if the prev item was not belong to this key
+					if(strcmp(item->key->row_key, prevItem->key->row_key) != 0){
+						break;
+					}else{
+						free_item(prevItem);
+						set->items[j] = NULL;
+					}
+				}
+				free_item(item);
+				set->items[i] = NULL;
+			}
+			//check if the item is the column name deletion mark,
+			//which will delete all item belong to this row key and column name before this item's timestamp
+			else if(item->val_len == 0){
+				int j=0;
+				for(j=i-1; j>=0; j--){
+					Item *prevItem = set->items[j];
+					if(prevItem == NULL) continue;
+					//The procedure will break if the prev item was not belong to this key
+					if(strcmp(item->key->row_key, prevItem->key->row_key) != 0){
+						break;
+					}else if(strcmp(item->key->column_name, prevItem->key->column_name) == 0){
+						free_item(prevItem);
+						set->items[j] = NULL;
+					}
+				}
+				free_item(item);
+				set->items[i] = NULL;
+			}
+		}
+		return set;
+}
+
 #ifdef ITEM_TEST
+void testcase_for_combine_and_cleansing_result_set(){
+		Item* item1 = m_create_item("1", "column_qualifier", "value");
+		Item* item2 = m_create_item("2", "column_qualifier", "value");
+		Item* item3 = m_create_item("2", "column_qualifier", NULL);
+		Item* item4 = m_create_item("3", "column_qualifier", "value");
+		Item* item5 = m_create_item("3", NULL, NULL);
+		Item* item6 = m_create_item("6", "column_qualifier", "value");
+		Item** items1 = malloc2(sizeof(Item*) * 4);
+		items1[0] = item1;
+		items1[1] = item2;
+		items1[2] = item3;
+		items1[3] = item4;
+		ResultSet* leftSet = m_create_result_set(4, items1);
+		Item** items2 = malloc2(sizeof(Item*) * 4);
+		items2[0] = item1;
+		items2[1] = item2;
+		items2[2] = item5;
+		items2[3] = item6;
+		ResultSet* rightSet = m_create_result_set(4, items2);
+		ResultSet* resultSet = m_combine_result_set(leftSet, rightSet);
+		printf("The ResultSet after combining.\n");
+		print_result_set_in_nice_format(resultSet);
+		resultSet = cleansing(resultSet);
+		printf("The ResultSet after cleansing.\n");
+		print_result_set_in_nice_format(resultSet);
+}
+
 void testcase_for_result_set_to_byte(void){
         int index = 2;
         Item** items = malloc2(sizeof(Item *) * index);
-        items[0] = m_create_item("row1", "column_family", "column_qualifier", "value");
-        items[1] = m_create_item("row2", "column_family", "column_qualifier", "value");
+        items[0] = m_create_item("row1", "column_qualifier", "value");
+        items[1] = m_create_item("row2",  "column_qualifier", "value");
         ResultSet* resultSet = m_create_result_set(index, items);
         Buf* buf = result_set_to_byte(resultSet);
         ResultSet* newSet = byte_to_result_set(get_buf_data(buf));
         printf("%s\n", newSet->magic);
         printf("%d\n", newSet->size);
         printf("%s\n", newSet->items[0]->value);
-        printf("%s\n", newSet->items[1]->key->column_family);
 }
 
 void testcase_for_qsort_item(void){
         int index = 4;
         Item** items = malloc2(sizeof(Item *) * index);
-        items[0] = m_create_item("b","","","");
-        items[1] = m_create_item("c","","","");
-        items[2] = m_create_item("d","","","");
-        items[3] = m_create_item("a","","","");
+        items[0] = m_create_item("b","","");
+        items[1] = m_create_item("c","","");
+        items[2] = m_create_item("d","","");
+        items[3] = m_create_item("a","","");
         qsort(items, index, sizeof(Item *), cmp_item);
         printf("%s\n",items[0]->key->row_key);
 }
@@ -367,7 +521,7 @@ void testcase_for_between_keys(void){
         int index = 3;
         Item** items = malloc2(sizeof(Item *) * 3);
         int i = 0;
-        for(i = 0; i < index; i++) items[i] = m_create_item(keys[i], "", "", "");
+        for(i = 0; i < index; i++) items[i] = m_create_item(keys[i], "", "");
         assert(!between_keys(items[0]->key, items[1]->key, keys[2]));
         assert(between_keys(items[0]->key, items[2]->key, keys[1]));
 }
@@ -377,7 +531,7 @@ void testcase_for_bsearch_item_index_by_row_key(void){
         int index = 5;
         Item** items = malloc2(sizeof(Item *) * index);
         int i = 0;
-        for(i = 0; i < index; i++) items[i] = m_create_item(tests[i], "", "", "");
+        for(i = 0; i < index; i++) items[i] = m_create_item(tests[i], "", "");
         printf("the result:%d\n", bsearch_item_index_by_row_key(0, index-1, items, tests[3]));
 }
 
@@ -391,14 +545,11 @@ void testcase_for_cmp_key_with_row_key(void){
         assert(cmp_key_with_row_key(key, test1)==0);
         assert(cmp_key_with_row_key(key, test2)!=0);
         assert(cmp_key_with_row_key(key, test3)!=0);
-
 }
 
 void print_item(Item *item){
-		printf("rowkey:%s\n", get_row_key_item(item));
-		printf("column_family:%s\n", get_column_family(item));
 		printf("column_qualifier:%s\n", get_column_name(item));
-		printf("time_stamp:%d\n", get_timestamp(item));
+		printf("time_stamp:%lld\n", get_timestamp(item));
 		printf("value:%s\n", get_value(item));
 		printf("\n");
 }
@@ -406,15 +557,13 @@ void print_item(Item *item){
 void testcase_for_result_set_serializing(void){
         ResultSet* resultSet1 = malloc2(sizeof(ResultSet));
         char *row_key1 = "m1";
-        char *column_family1 = "cf1";
         char *column_qualifier1 = "name";
         char *value1 = "ike";
         char *row_key2 = "m2";
-        char *column_family2 = "cf2";
         char *column_qualifier2 = "name";
         char *value2 = "wu";
-        Item *item1 = m_create_item(row_key1, column_family1, column_qualifier1, value1);
-        Item *item2 = m_create_item(row_key2, column_family2, column_qualifier2, value2);
+        Item *item1 = m_create_item(row_key1, column_qualifier1, value1);
+        Item *item2 = m_create_item(row_key2, column_qualifier2, value2);
         resultSet1->size = 2;
         resultSet1->items = malloc2(sizeof(Item *) * 2);
         resultSet1->items[0] = item1;
@@ -443,7 +592,8 @@ void test_suite(void){
 }
 
 int main(int argc, char *argv[]){
-        test_suite();
-        return 1;
+        //test_suite();
+		testcase_for_combine_and_cleansing_result_set();
+		return 1;
 }
 #endif
