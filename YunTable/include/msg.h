dfab6eb9d716de2b0fcd53df4a35352e39058cf4
ikewu83 <ikewu83@a26f21ce-7369-1176-bc8b-9f905022af7f>
2011-01-25 14:40:22 +0000

    Ver 0.9 Update ERR_MSG_WRONG_ACTION_MSG, and add a new message for WRONG_ADD_CMD

--- YunTable/include/msg.h
+++ YunTable/include/msg.h
@@ -8,7 +8,7 @@
 /** Error Messages **/
 #define ERR_MSG_CMD_NOT_COMPLETE "Please input the complete cmd."
 #define ERR_MSG_NULL_STRING "The input string is NULL or invalid."
-#define ERR_MSG_WRONG_ACTION "The action you just input is not supported."
+#define ERR_MSG_WRONG_ACTION "The action you just input is not supported. Please type \"help\" for more information."
 #define ERR_MSG_NO_COLUMN "No column has been inputed."
 #define ERR_MSG_NO_TABLE_NAME "No table name has been inputed."
 #define ERR_MSG_NO_ROW_KEY "Should have a row key."
@@ -18,6 +18,7 @@
 #define ERR_MSG_CLUSTER_FULL "Can not get a new region for this table because the whole cluster is full."
 #define ERR_MSG_WRONG_MASTER "Either the inputted master is wrong or has some connection problem."
 #define ERR_MSG_WRONG_REGION "Either the inputted region is wrong, has some connection problem or already existed."
+#define ERR_MSG_WRONG_ADD_CMD "The Add Command is not valid, for example, the table name contains space"
 
 /** Issue Messages **/
 #define ISSUE_MSG_NOTHING_FOUND "Nothing has been found."
