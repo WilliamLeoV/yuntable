57a3122405f58ae85c5040a302942e832f5b4263
ikewu83 <ikewu83@a26f21ce-7369-1176-bc8b-9f905022af7f>
2011-02-25 05:21:28 +0000

    Add a long-term testing mode.

--- trunk/testcase.sh
+++ trunk/testcase.sh
@@ -1,3 +1,26 @@
+test_routine(){
+	echo ""
+	echo "Staring the Test Routine"
+	echo ""
+	./yuncli -cmd put table:people row:me name:"ike" sex:"male"
+	echo ""
+	./yuncli -cmd put table:people row:me1 name:"ikea" sex:"female"
+	echo ""
+	./yuncli -cmd get table:people row:me
+	echo ""
+	./yuncli -cmd show table:people
+	echo ""
+	./yuncli -cmd del table:people row:me
+	echo ""
+	./yuncli -cmd del table:people row:me1 sex
+	echo ""
+	./yuncli -cmd get table:people
+	echo ""
+	echo "The Test Routine is end"
+	echo ""
+}
+
+
 echo "Starting the testcase script for YunTable"
 
 echo "Step 1, Unit Test"
@@ -24,32 +47,26 @@ echo "\n"
 ./yfileTest
 echo "\n\n"
 echo "Step 2, Short Test Case"
-echo "Step 2-1, Compiling Executable"
-sh build.sh
 echo "\n"
-echo "Step 2-2, Set up yuncli"
+echo "Step 2-1, Set up yuncli"
 ./yuncli -cmd add master:127.0.0.1:8301
 echo ""
 ./yuncli -cmd add region:127.0.0.1:8302
 echo "\n"
-echo "Step 2-3 Short Test Case1: People Table"
+echo "Step 2-2 Short Test Case1: People Table"
 echo "This Short Test Case is focusing the functionality"
 ./yuncli -cmd add table:people
 echo ""
 ./yuncli -cmd show master
-echo ""
-./yuncli -cmd put table:people row:me name:"ike" sex:"male"
-echo ""
-./yuncli -cmd put table:people row:me1 name:"ikea" sex:"female"
-echo ""
-./yuncli -cmd get table:people row:me
-echo ""
-./yuncli -cmd show table:people
-echo ""
-./yuncli -cmd del table:people row:me
-echo ""
-./yuncli -cmd del table:people row:me1 sex
-echo ""
-./yuncli -cmd get table:people
+
+test_routine
+
+echo "Step 3 Long Test Case"
+
+while true;
+do
+sleep 10
+test_routine
+done
 
 echo "All the testcases have been "
